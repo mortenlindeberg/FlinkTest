b15ad1ef484618ede542be1b4bb49bc570ff6519
mglindeb <mglindeb@ifi.uio.no>
2018-09-10 16:03:53 +0200

    Towards elasticsearch integration

--- src/main/java/no/uio/ifi/dmms/Consumer.java
+++ src/main/java/no/uio/ifi/dmms/Consumer.java
@@ -1,12 +1,22 @@
 package no.uio.ifi.dmms;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkFunction;
+import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
+import org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink;
 import org.apache.flink.util.Collector;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.Requests;
+
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 public class Consumer extends Thread {
     private int port;
@@ -29,6 +39,23 @@ public class Consumer extends Thread {
                         .max(1);
 
         stream.print();
+
+
+        List<HttpHost> httpHosts = new ArrayList<>();
+        httpHosts.add(new HttpHost("127.0.0.1", 9200, "http"));
+
+        ElasticsearchSinkFunction<Tuple2<Long, Double>> sinkFunction = new ElasticsearchSinkFunction<Tuple2<Long, Double>>() {
+            @Override
+            public void process(Tuple2<Long, Double> tuple, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
+                Map<String,String> json = new HashMap<>();
+                json.put("time",""+tuple.f0);
+                json.put("power",""+tuple.f1);
+                requestIndexer.add(Requests.indexRequest().index("gpx").type("float").source(json));
+            }
+        };
+        ElasticsearchSink.Builder<Tuple2<Long, Double>> esSinkBuilder = new ElasticsearchSink.Builder<>(httpHosts, sinkFunction);
+        stream.addSink(esSinkBuilder.build());
+
 
         // execute program
         try {
